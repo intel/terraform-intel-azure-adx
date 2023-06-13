b678a0f6451ed88adf9cfeb6ed368b48441f0824
Dave Shrestha <dave.shrestha@intel.com>
2023-06-13 09:53:48 -0700

    Update main.tf
    added "/intel" for source

--- examples/intel_adx_database_principal_assignment/main.tf
+++ examples/intel_adx_database_principal_assignment/main.tf
@@ -1,7 +1,7 @@
 ## This module deploys a Azure Data Explorer with assigned pricipal in a user provided resource group
 
 module "azure-dataexplorer" {
-  source                = "intel/azure-adx"
+  source                = "intel/azure-adx/intel"
   resource_group_name   = "DS-KUSTO-RG1"
   adx_sku               = "Standard_E8d_v5"
   principal_id          = "user@company.com"
